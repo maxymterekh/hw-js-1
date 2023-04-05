02cf8bb32707754b70b379d9afc2b4e38c0a236c
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-04-05 22:18:06 +0300

    hw 3 done

---
+++ js/index-hw-2.js
@@ -0,0 +1,65 @@
+
+// task 3
+
+const userName = prompt("Напишіть ваше ім'я")
+console.log(userName)
+
+// task 4
+
+const myAge = 14;
+const myFriendsAge = 14;
+alert(myAge + myFriendsAge)
+
+
+// task 5
+
+
+const text = "Я навчаюся в GoITeens в групі:";
+const group = "FE1y_1" ; 
+console.log(text,group)
+
+
+const str = "hello";
+const num = 123;
+const flag = true;
+const txt = "true";
+
+console.log(typeof str) 
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)
+
+// task 6
+
+const name = "MacBook Pro 13,";
+let price = "ціна: 10000";
+const chosed = "Обрано";
+const currency = "кредитів"
+
+console.log(chosed,name,price,currency)
+
+const conf = confirm("Змінити ціну на 20000 кредитів?")
+console.log(conf)
+
+if(conf === true) {
+     price = "ціна: 20000";
+}
+
+console.log(chosed,name,price,currency)
+
+
+// task 2
+
+let userTitle = "Tyler"; 
+let cost = 50; 
+let description = "Green, sharp, black"; 
+let isLoggedin = true;
+let hasChildren = false;
+
+const a = 200;
+const b = 20*50;
+const result = a + b;
+const title = "Tacos";
+const isModalopen = false;
+
+
---
+++ js/index-hw-3.js
@@ -0,0 +1,44 @@
+// task 1
+
+const k1 = "5px" ; 
+const k2 = "12djd334" ; 
+const k3 = "12.45asdwe12" ; 
+const k4 = "qwqweeewq" ; 
+
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+
+// task 2  
+
+
+const g1 = "5px" ; 
+const g2 = "12djd334" ; 
+const g3 = "12.45asdwe12" ; 
+const g4 = "qwqweeewq" ; 
+
+console.log(Number.parseFloat(g1));
+console.log(Number.parseFloat(g2));
+console.log(Number.parseFloat(g3));
+console.log(Number.parseFloat(g4));
+
+// task 3
+
+
+
+const biggestNum = Math.max(2, 34, 99, 3, 22, 36, 733, 18)
+console.log(biggestNum)
+
+const smallestNum = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+console.log(smallestNum)
+
+
+// task 4 
+
+const randomNum = Math.round(Math.random() *(19-3) + 3 )
+console.log(randomNum)
+
+
+
+
