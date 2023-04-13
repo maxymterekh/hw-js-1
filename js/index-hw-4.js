8f654f1f47f642d729e22b5667aa48f26851f50d
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-04-13 21:59:34 +0300

    hw 4

---
+++ js/index-hw-4.js
@@ -0,0 +1,44 @@
+// task 1
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = (pricePerItem * orderedQuantity);
+console.log(totalPrice)
+
+// task  2
+
+const age = 12;
+const adultAge = 18;
+const isAdult = (age >= adultAge)
+console.log(isAdult)
+
+// task 3
+
+const SAVED_PASSWORD = "password";
+const currentPassword = "paccwort";
+const isMatch = (SAVED_PASSWORD === currentPassword);
+console.log(isMatch)
+
+// task 4
+
+let a = 5;
+
+a = (a += 2);
+ 
+
+let b = 10;
+
+b = (b -= 4);
+
+
+let c = 15;
+
+
+c = (c *= 3);
+
+
+let d = 20;
+
+d = (d /= 10);
+
+console.log(a , b , c , d);
+
