baa17d5108c543c9b5b717e6371c2c881f8d233c
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-04-28 22:19:34 +0300

    hw 6 done

---
+++ js/index-hw-6.js
@@ -0,0 +1,139 @@
+
+// task 1
+// const bank = 500000;
+
+
+// if (true) {
+//     const bankAccount = 10000;
+//     console.log(bank)
+//     if (true) {
+//         const quantity = 50;
+
+//     }
+// }
+
+// task 2
+
+// const realName = "ECMAScript";
+// const question = prompt("Яка “офіційна” назва JavaScript?");
+
+// if (realName === question) {
+//     console.log("Правильно!")
+// }
+// else {
+//     console.log("Ви не знаєте? ECMAScript!")
+// }
+
+// task 3
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 45; 
+
+// if (number >= x1, number <= x2) {
+//     console.log("у проміжку");
+// }
+// else if (number <= x1) {
+//     console.log("до проміжку");
+// }
+// else if (number >= x2) {
+//     console.log("після проміжку")
+// }
+
+// task 4
+
+// const isOnline = false;
+// const isFriend = true;
+// const isDnd = false;
+
+// if (isOnline === true && isFriend === true && isDnd === true) {
+//     console.log("you can chat with this person")
+// }
+// else (
+//     console.log("you cant chat with this person")
+// )
+
+// task 5
+
+// const rank = "pro"
+
+// if (rank === "vip" || rank === "pro") {
+//     console.log("Вітаю, ось ваш контент: Морфологічний розбір — розбір, покликаний посприяти засвоєнню і систематизації ознак частин мови, їхніх граматичних категорій, виробленню міцних практичних навичок словозміни іменних слів і дієслова, підготовці до вивчення синтаксису як учення про зв'язки слів у словосполученні й реченні.")
+// }
+// else {
+//     console.log("На жаль тип вашої підписки не включає цей контент! :(")
+// }
+
+// task 6
+
+
+// const number = prompt("Введіть число!")
+
+// if (number >= 55 && number <= 99) {
+//     console.log("Число потрапляє в діапазон")
+// }
+// else {
+//     console.log("Число не потрапляє в діапазон")
+// }
+
+// task 7
+
+// const maxymAge = 55;
+
+// if (maxymAge >= 0 && maxymAge <= 16) {
+//     console.log("children")
+// }
+// else if (maxymAge >= 17 && maxymAge <= 60) {
+//     console.log("adult")
+// }
+// else if (maxymAge >= 61 && maxymAge <= 100) {
+//     console.log("retired")
+// }
+// else {
+//     console.log("ви гуманоїд")
+// }
+
+// task 8
+
+// const userName = "maxym";
+// const userLastName = "terekh";
+// const nameLength = userName.length;
+// const lastNameLength = userLastName.length;
+// console.log(typeof nameLength)
+
+// if (nameLength >= 4 && lastNameLength >= 5) {
+//     const summa = nameLength + lastNameLength ; 
+//     console.log(summa)
+// }
+// else {
+//     console.log("ОЙ, неможливо виконати умову")
+// }
+
+// task 9
+
+// let result;
+// const a = 1;
+// const b = 2;
+
+// result = a + b < 4 ? "вище" : "нижче" ;
+// console.log(result)
+
+// task 10
+
+// if ("0") {
+
+
+//     console.log( 'Привіт' );
+   
+   
+//    }
+
+
+// в консолі тру, бо рядо нуль рахується як тру, бо будь-який рядок крім пустого рахується за тру 
+
+
+
+
+
+
+
