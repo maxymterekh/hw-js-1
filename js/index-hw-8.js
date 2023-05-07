9b56a1e4fe69f62769e66da19ccf3c4547a93aee
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-05-07 17:50:20 +0300

    hw 8 done

---
+++ js/index-hw-8.js
@@ -0,0 +1,78 @@
+// task 1
+
+// const nums = [1, 9, 78]
+
+// nums[1] = 10;
+
+
+
+// task 2
+
+// const strings = ["m", "w", "2"];
+
+// const randomString = "131323";
+
+// strings.push(randomString)
+
+// console.table(strings)
+
+// task 3
+
+// const numbers = [1, 5, 3, 7, 8];
+// let sum = 0;
+
+// for(let i = 0; i < numbers.length; i++) {
+//      sum += numbers[i];
+// }
+
+// console.log(sum)
+
+// task 4
+
+// const realNumbers = [4, 6, 9, 0, 2];
+
+// for(let i = 0; i < realNumbers.length; i++) {
+//     console.log(realNumbers[i])
+// }
+
+// task 5
+
+// const realStrings = ["mango", "apple", "banana", "kiwi", "op"] ;
+
+// for(let i = 0; i < realStrings.length; i++) {
+//     if(5 < realStrings[i].length) {
+//       console.log(realStrings[i])
+//     }
+// }
+
+// task 6
+
+// const array = [1, 629830294, 7, 3, 123, 253, 87, 7897, 123124515, 43];
+// let max = array[0];
+
+// for(let i = 0; i < array.length; i++) {
+//     if(array[i] > max) {
+//         max = array[i];
+//     }
+// }
+// console.log(max)
+
+// task 7
+
+// const evenNumbers = [13, 24, 645883, 576578934, 121214, 768987946, 45623647, 43234242, 6734355454577, 7888787454];
+
+
+// for(let i = 0; i < evenNumbers.length; i++) {
+//     if (evenNumbers[i] % 2 === 0  ) {
+//         console.log(evenNumbers[i])
+//     }
+// }
+
+
+
+
+
+
+
+
+
