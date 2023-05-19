0fbbf2abf06becd9edd007fb9e046806f1d637f8
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-05-19 20:08:11 +0300

    hw 9

---
+++ js/index-hw-9.js
@@ -0,0 +1,77 @@
+// // task 1
+
+// // const arr = [1, 2, 3];
+// // arr.push(4, 5, 6);
+// // console.log(arr); 
+
+// // task 2
+
+// const massive = [1, 2, 3];
+// massive.unshift(4, 5, 6);
+// console.log(massive);
+
+// task 3
+
+// const lang = ['js', 'css', 'jq'];
+// console.log(lang[0])
+
+// task 4
+
+// const mas = ['js', 'css', 'jq'];
+// console.log(mas[mas.length - 1]);
+
+// task 5
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.slice(0, 3));
+
+// task 6
+
+// const numbers =  [1, 2, 3, 4, 5];
+// console.log(numbers.slice(3, 5))
+
+// task 7 
+
+
+// let fruits = ['яблуко', 'груша', 'банан', 'апельсин', 'киві'];
+
+// // завдання 1
+// fruits.shift(); 
+// fruits.pop(); 
+// fruits.unshift('ананас'); 
+// fruits.push('манго'); 
+// console.log(fruits); 
+
+// // завдання 2
+// fruits.splice(2, 1, 'виноград', 'слива', 'персик');
+
+// console.log(fruits); 
+
+// // завдання 3
+// const message = `У нас є наступні фрукти: ${fruits.join(', ')}.`;
+// console.log(message); 
+
+// task 8
+
+
+// let styles = ["jazz", "bluz"]
+
+// styles.push("rock n roll")
+
+// styles[Math.floor(styles.length / 2)] = 'Класика';
+
+// const firstElement = styles.shift();
+// console.log(firstElement)
+
+// styles.unshift("rap", "reggi")
+
+// console.table(styles)
+
+
+
+
+
+
+
+
+
